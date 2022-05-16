d1ed04bf6b125f9110a13eb4161b0f6f054ba5b3
Nathan Mohapatra <nathan.mohapatra@gmail.com>
2022-05-16 09:55:57 -0700

    Python P1 Revision

--- python/assignments.py
+++ python/assignments.py
@@ -3,44 +3,33 @@ import json
 # 1. Create a script that will read and parse the given files and remove duplicates using python, 
 # then write back into a single CSV
 def get_csv(infile):
-    """
-    Parses file, cleans data, and appends unique lines to csv file
-    
-    :infile: Input file
-    """
-    next(infile)  # skip header
-    for line in infile:
-        line = line.split()
+    with open('./python/io/people.csv', 'a', encoding='utf-8') as outfile:
+        next(infile)  # skip header
+        for line in infile:
+            line = line.split()
 
-        first, last = line[0], line[1]
-        line[0], line[1] = first.title(), last.title()
+            first, last = line[0], line[1]
+            line[0], line[1] = first.title(), last.title()
 
-        phone = line[3]
-        if len(phone) == 10:
-            line[3] = '-'.join([phone[:3], phone[3:6], phone[6:]])
+            phone = line[3]
+            if len(phone) == 10:
+                line[3] = '-'.join([phone[:3], phone[3:6], phone[6:]])
 
-        address_no = line[4]
-        line[4] = address_no[-4:]
+            address_no = line[4]
+            line[4] = address_no[-4:]
 
-        line = ','.join(line)  # csv
-        if line not in unique_lines:
-            unique_lines.add(line)
-            outfile.write(line + '\n')
+            line = ','.join(line)  # csv
+            if line not in unique_lines:
+                unique_lines.add(line)
+                outfile.write(line + '\n')
 
 unique_lines = set()  # to ignore duplicates
 
-OUTFILE_PATH = './python/io/people.csv'
-outfile = open(OUTFILE_PATH, 'a', encoding='utf-8')
+with open('./python/io/people_1.txt', 'r', encoding='utf-8') as infile1:
+    get_csv(infile1)
 
-INFILE_PATH = './python/io/people_1.txt'
-with open(INFILE_PATH, 'r', encoding='utf-8') as infile:
-    get_csv(infile)
-
-INFILE_PATH = './python/io/people_2.txt'
-with open(INFILE_PATH, 'r', encoding='utf-8') as infile:
-    get_csv(infile)
-        
-outfile.close()
+with open('./python/io/people_2.txt', 'r', encoding='utf-8') as infile2:
+    get_csv(infile2)
 
 
 # 2. Split movie.json into 8 smaller JSON files.
